ddde1c04fb6efdc1ad2f4c7fbc7c8d9e1ebc1f98
Daniel Arbon <danieltarbon@live.com.au>
2023-11-09 16:23:37 +1030

    use clsx to style active page link blue

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -1,9 +1,12 @@
+'use client'; // React 'use client' directive
 import {
   UserGroupIcon,
   HomeIcon,
   DocumentDuplicateIcon,
 } from '@heroicons/react/24/outline';
+import clsx from 'clsx'; // import clsx for conditional styling
 import Link from 'next/link';  //import nextjs Link component
+import { usePathname } from 'next/navigation'; // import usePathname to use url path in code
 
 // Map of links to display in the side navigation.
 // Depending on the size of the application, this would be stored in a database.
@@ -18,6 +21,7 @@ const links = [
 ];
 
 export default function NavLinks() {
+  const pathname = usePathname(); // get pathname from usePathname;
   return (
     <>
       {links.map((link) => {
@@ -27,7 +31,9 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+            // use clsx to conditionally color link blue if it matches the current pathname
+            className={clsx("flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3",
+            { 'bg-sky-100 text-blue-600' : pathname===link.href})}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
